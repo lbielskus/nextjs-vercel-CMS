bdc27f8186fecf1f911e5fab86e712a67027e935
LBielskus <liudbiel@gmail.com>
2024-03-25 13:44:50 +0200

    6

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'], // Add res.cloudinary.com here
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
   },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
+  exclude: [
+    '/pages/components/Button/ButtonElements.ts',
+
+    '/pages/components/Button/ButtonElements.ts',
+    '/pages/components/Form/FormElements.ts',
+    '/pages/components/Form/FormmElements.ts',
+    '/pages/components/Form/InputFieldElements.ts',
+    'pages/helpers/',
+    '/pages/types/error.ts',
+    '/pages/types/',
+    '/pages/types/propTypes.ts',
+
+    '/pages/utils/dbUtils/',
+  ],
 };
 
 module.exports = nextConfig;
