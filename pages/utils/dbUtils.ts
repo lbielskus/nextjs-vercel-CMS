3ba7182164e17039b4ca8838f4adafd35d236891
LBielskus <liudbiel@gmail.com>
2024-03-25 13:22:45 +0200

    dbUtils changes

--- pages/utils/dbUtils.ts
+++ pages/utils/dbUtils.ts
@@ -2,3 +2,8 @@ export async function createItem(data: any): Promise<string> {
   console.log('Creating item with data:', data);
   return 'mock-item-id';
 }
+
+// Define dbConnect function
+export async function dbConnect() {
+  // Logic for connecting to your database
+}
